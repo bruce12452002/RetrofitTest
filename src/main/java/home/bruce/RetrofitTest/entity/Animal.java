eb0bf3eac22ade8d731711b0f3a6fcd3607f8fe9
DESKTOP-OS88KVE\bruce <brucec@innotech.me>
2022-08-03 15:19:36 +0800

    del annotation

--- src/main/java/home/bruce/RetrofitTest/entity/Animal.java
+++ src/main/java/home/bruce/RetrofitTest/entity/Animal.java
@@ -1,14 +1,11 @@
 package home.bruce.RetrofitTest.entity;
 
-import com.google.gson.annotations.SerializedName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Animal {
-//    @SerializedName("id")
     private int id;
-
     private String name;
 }
